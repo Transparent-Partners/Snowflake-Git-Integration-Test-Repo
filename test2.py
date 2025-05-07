8f55b15d9c44378543d32c1f5faf51febeef48cc
bdeng-tp <benjamin.deng@transparent.partners>
2025-05-07 16:17:06 -0500

    Update3 test2.py

--- test2.py
+++ test2.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
-df = pd.read_snowflake('SYNTHETIC_PROFILE_500K')
+snowpark_df = session.table('SYNTHETIC_PROFILE_500K')
+
+df = snowpark_df.to_pandas()
 test_df = df[['VISITOR_ID', 'VISITOR_CREATED', 'PROPERTY_COUNTRY', 'BADGE_EMAIL_OPTED_IN']]
 filtered_test_df = test_df[(test_df['PROPERTY_COUNTRY'] == 'US') & (test_df['BADGE_EMAIL_OPTED_IN'] == 'TRUE')]
+filter_test_df.head()
