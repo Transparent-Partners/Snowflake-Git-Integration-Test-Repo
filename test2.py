3aeda29cac677bb676774b30d9a381fb523e0ce7
bdeng-tp <benjamin.deng@transparent.partners>
2025-05-07 16:12:01 -0500

    Update2 test2.py

--- test2.py
+++ test2.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 df = pd.read_snowflake('SYNTHETIC_PROFILE_500K')
 test_df = df[['VISITOR_ID', 'VISITOR_CREATED', 'PROPERTY_COUNTRY', 'BADGE_EMAIL_OPTED_IN']]
-filtered_test_df = test_df[(test_df['PROPERTY_COUNTRY' == 'US') & (TEST_DF['BADGE_EMAIL_OPTED_IN'] == 'TRUE')]
+filtered_test_df = test_df[(test_df['PROPERTY_COUNTRY'] == 'US') & (test_df['BADGE_EMAIL_OPTED_IN'] == 'TRUE')]
