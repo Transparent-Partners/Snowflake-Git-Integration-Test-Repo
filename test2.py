667ca2025fd9b214308b6289750588ed50c8dd03
bdeng-tp <benjamin.deng@transparent.partners>
2025-05-07 14:57:53 -0500

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.DataFrame(CHAMBERLAIN.UNSUBSCRIBES)
+test_df = df[['VISITOR_ID', 'VISITOR_CREATED', 'PROPERTY_COUNTRY', 'BADGE_EMAIL_OPTED_IN']]
+filtered_test_df = test_df[(test_df['PROPERTY_COUNTRY' == 'US') & (TEST_DF['BADGE_EMAIL_OPTED_IN'] == 'TRUE')]
